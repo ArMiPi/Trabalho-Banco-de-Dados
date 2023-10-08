e5199f85831532abc630bb5d0a2d1b9bf185a5bc
ArMiPi <arthur.miasatopimont@uel.br>
2023-10-08 20:35:07 -0300

    Correções modelo ER
    	- Modelo-Entidade-Relacionamento
    		Foram removidas tabelas que não faziam sentido para o
    		objetivo do trabalho, e também alguns registros cujos
    		dados fornecido pela API não seriam úteis para o
    		trabalho
    
    	- Esquema-Relacional
    		Foram feitos ajustes para que o esquema representasse de
    		forma fiel o modelo
    
    	- db
    		Foram feitas as alterações para que db represente o
    		esquema
    
    		BIT foi substituído por SMALLINT

--- scripts/db.sql
+++ scripts/db.sql
@@ -14,11 +14,17 @@ CREATE TABLE pokemon (
     rarity CHAR,
     sprite VARCHAR(200),
     id_generation INT,
+    evolves_from INT,
     CONSTRAINT pk_pokemon PRIMARY KEY(id_pokedex),
     CONSTRAINT fk_generation FOREIGN KEY(id_generation)
         REFERENCES generation(id_generation)
         ON UPDATE CASCADE
         ON DELETE SET NULL,
+    CONSTRAINT fk_evolves_from FOREIGN KEY(evolves_from)
+        REFERENCES pokemon(id_pokedex)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT ck_evolves CHECK(evolves_from <> id_pokedex),
     CONSTRAINT unique_name_pokemon UNIQUE(name),
     CONSTRAINT ck_height CHECK(height > 0),
     CONSTRAINT ck_weight CHECK(weight > 0),
@@ -28,7 +34,7 @@ CREATE TABLE pokemon (
 CREATE TABLE ability (
     id_ability INT,
     name VARCHAR(100) NOT NULL,
-    is_hidden BIT DEFAULT '0'::BIT NOT NULL,
+    is_hidden SMALLINT DEFAULT 0 NOT NULL,
     CONSTRAINT pk_ability PRIMARY KEY(id_ability),
     CONSTRAINT unique_name_ability UNIQUE(name)
 );
@@ -50,19 +56,15 @@ CREATE TABLE have_ability (
 CREATE TABLE pokemon_go (
     id_pokemon_go INT,
     id_pokemon INT NOT NULL,
-    raid_exclusive BIT DEFAULT '0'::BIT NOT NULL,
+    raid_exclusive SMALLINT DEFAULT 0 NOT NULL,
     max_cp INT,
     buddy_distance INT,
     candy_to_evolve INT,
-    capture_rate FLOAT,
-    flee_rate FLOAT,
     CONSTRAINT pk_pokemon_go PRIMARY KEY(id_pokemon_go),
     CONSTRAINT fk_pokemon FOREIGN KEY(id_pokemon)
         REFERENCES pokemon(id_pokedex)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    CONSTRAINT ck_capture_rate CHECK(capture_rate >= 0.0 AND capture_rate <= 1.0),
-    CONSTRAINT ck_flee_rate CHECK(flee_rate >= 0.0 AND flee_rate <= 1.0),
     CONSTRAINT ck_max_cp CHECK(max_cp >= 0),
     CONSTRAINT ck_buddy_distance CHECK(buddy_distance >= 0),
     CONSTRAINT ck_candy_to_evolve CHECK(candy_to_evolve >= 0)
@@ -71,9 +73,9 @@ CREATE TABLE pokemon_go (
 CREATE TABLE shiny (
     id_shiny INT,
     id_pokemon INT NOT NULL,
-    egg BIT DEFAULT '0'::BIT NOT NULL,
-    raid BIT DEFAULT '0'::BIT NOT NULL,
-    wild BIT DEFAULT '0'::BIT NOT NULL,
+    egg SMALLINT DEFAULT 0 NOT NULL,
+    raid SMALLINT DEFAULT 0 NOT NULL,
+    wild SMALLINT DEFAULT 0 NOT NULL,
     sprite VARCHAR(200),
     CONSTRAINT pk_shiny PRIMARY KEY(id_shiny),
     CONSTRAINT fk_pokemon_go FOREIGN KEY(id_pokemon)
@@ -103,50 +105,6 @@ CREATE TABLE type (
     name VARCHAR(50) NOT NULL,
     CONSTRAINT pk_type PRIMARY KEY(id_type),
     CONSTRAINT unique_name_type UNIQUE(name)
-);
-
-CREATE TABLE go_move (
-    id_move INT,
-    category VARCHAR(10),
-    name VARCHAR(50) NOT NULL,
-    power INT,
-    id_type INT NOT NULL,
-    CONSTRAINT pk_id_go_move PRIMARY KEY(id_move),
-    CONSTRAINT fk_type FOREIGN KEY(id_type)
-        REFERENCES type(id_type)
-        ON UPDATE CASCADE
-        ON DELETE SET NULL,
-    CONSTRAINT ck_power CHECK(power > 0),
-    CONSTRAINT unique_name_go_move UNIQUE(name)
-);
-
-CREATE TABLE go_learn (
-    id_pokemon INT,
-    id_move INT,
-    CONSTRAINT pk_go_learn PRIMARY KEY(id_pokemon, id_move),
-    CONSTRAINT fk_pokemon_go FOREIGN KEY(id_pokemon)
-        REFERENCES pokemon_go(id_pokemon_go)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    CONSTRAINT fk_move FOREIGN KEY(id_move)
-        REFERENCES go_move(id_move)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
-
-CREATE TABLE evolves (
-    original_form INT,
-    evolution INT,
-    CONSTRAINT pk_evolves PRIMARY KEY(original_form, evolution),
-    CONSTRAINT fk_original_id FOREIGN KEY(original_form)
-        REFERENCES pokemon(id_pokedex)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    CONSTRAINT fk_evolution FOREIGN KEY(evolution)
-        REFERENCES pokemon(id_pokedex)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    CONSTRAINT ck_evolution CHECK(original_form <> evolution)
 );
 
 CREATE TABLE base_stats (
@@ -205,9 +163,15 @@ CREATE TABLE learn_move (
 );
 
 CREATE TABLE type_relations (
-    id_stronger INT,
-    id_weaker INT,
+    id_attacker INT,
+    id_defender INT,
     multiplier FLOAT,
-    CONSTRAINT pk_type_relations PRIMARY KEY(id_stronger, id_weaker),
+    CONSTRAINT pk_type_relations PRIMARY KEY(id_attacker, id_defender),
     CONSTRAINT ck_multiplier CHECK(multiplier >= 0)
+);
+
+CREATE TABLE is_of_type (
+    id_pokemon INT,
+    id_type INT,
+    CONSTRAINT pk_is_of_type PRIMARY KEY(id_pokemon, id_type)
 );

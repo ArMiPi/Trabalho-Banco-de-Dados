c86504407a56df3c2bb92d8d92263b4b1d25f449
ArMiPi <arthur.miasatopimont@uel.br>
2023-11-21 08:40:57 -0300

    Adicionadas funções para fazer requisição para a API do pokemon go

--- Back/src/main/java/uel/bd/Bulbapedia/utils/APIRequests.java
+++ Back/src/main/java/uel/bd/Bulbapedia/utils/APIRequests.java
@@ -1,5 +1,6 @@
 package uel.bd.Bulbapedia.utils;
 
+import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONObject;
 
@@ -21,6 +22,46 @@ public class APIRequests {
 
             JSONParser parser = new JSONParser();
             return (JSONObject) parser.parse(response.body());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public static JSONObject getGoAPIResponse(String url) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(url))
+                    .header("X-RapidAPI-Key", "804f1de5d6msh5095431ee02db67p1cd1c7jsnf09f38d86210")
+                    .header("X-RapidAPI-Host", "pokemon-go1.p.rapidapi.com")
+                    .GET()
+                    .build();
+
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            JSONParser parser = new JSONParser();
+            return (JSONObject) parser.parse(response.body());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public static JSONArray getGoAPIResponseAsArray(String url) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(url))
+                    .header("X-RapidAPI-Key", "804f1de5d6msh5095431ee02db67p1cd1c7jsnf09f38d86210")
+                    .header("X-RapidAPI-Host", "pokemon-go1.p.rapidapi.com")
+                    .GET()
+                    .build();
+
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            JSONParser parser = new JSONParser();
+            return (JSONArray) parser.parse(response.body());
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;

d4975b9edbf383d48ea4e5f2355adfd484ce81b8
ArMiPi <arthur.miasatopimont@uel.br>
2023-12-07 15:57:05 -0300

    Nova versão da função que obtém os dados dos Pokemons

--- Back/src/main/java/uel/bd/Bulbapedia/DAO/PokemonJdbcDAO.java
+++ Back/src/main/java/uel/bd/Bulbapedia/DAO/PokemonJdbcDAO.java
@@ -45,19 +45,31 @@ public class PokemonJdbcDAO implements DAO<Pokemon>{
 
     private static final String GET_POKEMON_GENERAL_DATA =
             """
-                    SELECT
-                    pk.*,
-                    bs.hp, bs.attack, bs.defense, bs.sp_attack, bs.sp_defense, bs.speed,
-                    pk_go.raid_exclusive, pk_go.max_cp, pk_go.buddy_distance, pk_go.candy_to_evolve,
-                    bs_go.stamina AS go_stamina, bs_go.defense AS go_defense, bs_go.attack AS go_attack,
-                    shiny.egg, shiny.raid, shiny.wild, shiny.sprite
-                    FROM pokemon AS pk
-                    LEFT JOIN base_stats AS bs ON pk.id_pokedex = bs.id_pokemon
-                    LEFT JOIN pokemon_go AS pk_go ON pk.id_pokedex = pk_go.id_pokemon
-                    LEFT JOIN base_go_stats AS bs_go ON pk.id_pokedex = bs_go.id_pokemon
-                    LEFT JOIN shiny ON pk.id_pokedex = shiny.id_pokemon
-                    WHERE id_pokedex = ?    
-                    """;
+                SELECT
+                	pk.*,
+                	tp.name AS type_name, tp.slot,
+                	bs.hp, bs.attack, bs.defense, bs.sp_attack, bs.sp_defense, bs.speed,
+                	hab.name AS ability_name, hab.is_hidden,
+                	pk_go.raid_exclusive, pk_go.max_cp, pk_go.buddy_distance, pk_go.candy_to_evolve,
+                	shiny.egg, shiny.raid, shiny.wild, shiny.sprite AS sprite_shiny,
+                	bs_go.stamina AS go_stamina, bs_go.defense AS go_defense, bs_go.attack AS go_attack
+                FROM pokemon AS pk
+                LEFT JOIN (
+                	SELECT iot.id_pokemon, type.name, iot.slot
+                	FROM is_of_type AS iot
+                	JOIN type ON iot.id_type = type.id_type
+                ) AS tp ON pk.id_pokedex = tp.id_pokemon
+                LEFT JOIN base_stats AS bs ON pk.id_pokedex = bs.id_pokemon
+                LEFT JOIN (
+                	SELECT ha.id_pokemon, ab.name, ha.is_hidden
+                	FROM have_ability AS ha
+                	LEFT JOIN ability AS ab ON ha.id_ability = ab.id_ability
+                ) AS hab ON pk.id_pokedex = hab.id_pokemon
+                LEFT JOIN pokemon_go AS pk_go ON pk.id_pokedex = pk_go.id_pokemon
+                LEFT JOIN shiny ON pk.id_pokedex = shiny.id_pokemon
+                LEFT JOIN base_go_stats AS bs_go ON pk.id_pokedex = bs_go.id_pokemon
+                WHERE id_pokedex = ?    
+            """;
     private static final String UPDATE_QUERY =
             """
                 UPDATE pokemon SET
@@ -100,9 +112,9 @@ public class PokemonJdbcDAO implements DAO<Pokemon>{
         }
     }
 
-    public Map<String, Object> getTest(Object id_pokedex) {
+    public List<Map<String, Object>> getPokemonGeneralData(Object id_pokedex) {
         try {
-            return template.queryForMap(GET_POKEMON_GENERAL_DATA, id_pokedex);
+            return template.queryForList(GET_POKEMON_GENERAL_DATA, id_pokedex);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
